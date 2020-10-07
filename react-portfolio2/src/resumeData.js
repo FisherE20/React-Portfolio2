2fc308de857ab1ff6907b43f3e872cacbbaeb228
Erin Fisher <flybirdie85@gmail.com>
2020-10-07 16:40:30 -0700

    all code for React Portfolio to see if it works

---
+++ react-portfolio2/src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "imagebaseurl":"https://FisherE20.github.io/",
+    "name": "Erin Fisher",
+    "role": "Frontend Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "facebookid": "Your facebookid",
+    "roleDescription": "I enjoy the creativity and design aspect of the frontend deelopment",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/erin-k-fisher/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/FisherE20/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Facebook",
+          "url":"https://www.facebook.com/erin.bird.370",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"Hello, I reside in Tucson, AZ with the loving and supportive husband Michael. I'm origanally from Rohnert Park, CA where I grew up with my 2 younger sisters and a younger brother. In my free time I enjoy cooking, baking, doing crafts (making wreaths) and hanging out with family and friends. I'm also a huge movie and music fan. I have worked in the dental field for the last 19 years and decided it was time for a career change. I decided to go into web development/coding because in highschool I took a semester of web design and really enjoyed myself. My dream job would be to work somewhere that both challenges me and allows me to be creative..",
+    "address":"Tucson, AZ",
+    "website":"https://FisherE20.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Arizona, Coding Bootcamp",
+        "specialization":"Web Development",
+      },
+      {
+        "UniversityName":"California State University, Chico",
+        "specialization":"Interior Design",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Las Casas Dental",
+        "specialization":"Patient Care Coordinator/ Dental Assistant",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2020",
+        
+      },
+      {
+        "CompanyName":"Dehnert Dental",
+        "specialization":"Dental Assistant",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2018",
+        
+      },{
+        "CompanyName":"William J. Bohan, D.M.D",
+        "specialization":"Front Office Manager",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2017",
+        
+      }
+
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"MYSQL"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Travel App",
+        "description":"mobileapp",
+        "imgurl":"Images/Portfolio/Travel.png"
+      },
+      {
+        "name":"COVID Trivia Night",
+        "description":"mobileapp",
+        "imgurl":"Images/Portfolio/COVID.png"
+      },
+      {
+        "name":"Eat-Da- Burger",
+        "description":"mobileapp",  
+        "imgurl":"Images/Portfolio/Eat-DA-Burger.png"
+      },
+      {
+        "name":"Note Taker",
+        "description":"mobileapp",
+        "imgurl":"Images/Portfolio/Note-taker.png"
+      },
+      {
+        "name":"Weather Dashboard",
+        "description":"mobileapp",
+        "imgurl":"Images/Portfolio/Weather.png"
+      },
+      {
+        "name":"Password Generator",
+        "description":"mobileapp",
+        "imgurl":"Images/Portfolio/Password.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Former Employer"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Former Employer"
+      }
+    ]
+  }
+  
+  export default resumeData
