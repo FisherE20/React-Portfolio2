a3d3ff97786356735ba4108e47e68b9c5096823e
Erin Fisher <flybirdie85@gmail.com>
2020-10-24 14:04:50 -0700

    trying something

---
+++ build/precache-manifest.0bbf05340e6de26db6d7e1f8d54a4c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39341b26603ed50dd5ec48276aff470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0cdddf68f64ec07400",
+    "url": "/static/css/main.20174c6a.chunk.css"
+  },
+  {
+    "revision": "dd34bc432f70ff3a4c58",
+    "url": "/static/js/2.82abcba1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.82abcba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0cdddf68f64ec07400",
+    "url": "/static/js/main.cbd646d3.chunk.js"
+  },
+  {
+    "revision": "5ba77631ee7b7fe1ae4f",
+    "url": "/static/js/runtime-main.51e2b022.js"
+  }
+]);
